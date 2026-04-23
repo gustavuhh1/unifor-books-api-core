7db7ba9cdcb4abe31375deb497b6814bf15fc6ec
Gustavo Martins Rodrigues <gustavomrodrigues11@gmail.com>
2026-04-23 06:38:01 -0300

    feat: aplicando o swagger no projeto

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,41 @@ import { usersRoutes } from "./modules/users/users.routes";
 import { authRoutes } from "./modules/auth/auth.routes";
 import { booksRoutes } from "./modules/books/books.routes";
 
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
 const app = Fastify({
   logger: process.env.NODE_ENV === "development",
 });
 
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Unifor Books API",
+      description: "API Core do Ecossistema Biblioteca Digital UNIFOR",
+      version: "1.0.0",
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "list",
+    deepLinking: false,
+  },
+});
+
+// É importante registrar o Swagger ANTES das rotas!
 app.register(usersRoutes);
 app.register(authRoutes);
 app.register(booksRoutes);
